db3eec570a446d1dfe1b20521561fb122fc05de5
Monarch Bhardwaj <Monarch@Mobility-Macbook-Pro.local>
2019-01-10 17:46:44 +0530

    first commit

---
+++ telstraPOCTests/telstraPOCTests.swift
@@ -0,0 +1,34 @@
+//
+//  telstraPOCTests.swift
+//  telstraPOCTests
+//
+//  Created by Monarch Bhardwaj on 10/01/19.
+//  Copyright © 2019 Monarch Bhardwaj. All rights reserved.
+//
+
+import XCTest
+@testable import telstraPOC
+
+class telstraPOCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
